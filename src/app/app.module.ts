91fb424c07986c6e170d3af0e1cd2ba3919b427c
Trend20 <enockomondi305@gmail.com>
2022-11-10 08:32:42 +0300

    project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MoviesComponent } from './component/movies/movies.component';
+import { MovieDetailsComponent } from './component/movie-details/movie-details.component';
+import { MovieCardListComponent } from './component/movie-card-list/movie-card-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent,
+    MovieDetailsComponent,
+    MovieCardListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
