ce3f748bf1efb3a1ec60082045aa30cecae11b23
Trend20 <enockomondi305@gmail.com>
2022-11-10 13:51:33 +0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { MovieDetailsComponent } from './component/movie-details/movie-details.component';
+import { MoviesComponent } from './component/movies/movies.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MoviesComponent},
+  {path: 'movies/:id', component: MovieDetailsComponent},
+  {path: '***', component: MoviesComponent, pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
